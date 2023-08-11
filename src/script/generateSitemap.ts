5ff13897acc8a627ada858d61714d9ee1be401b8
kulikov-debug <63264840+kulikov-debug@users.noreply.github.com>
2023-08-11 14:47:27 +0200

    Update generateSitemap.ts

--- src/script/generateSitemap.ts
+++ src/script/generateSitemap.ts
@@ -1,6 +1,9 @@
 import { scrapingURL } from './scrapingURLs';
 import { urlsToSitemap } from './urlsToSitemap';
 
+const URL = 'https://example.com'; // Replace with your desired URL
+const waitSec = 3; // Replace with your desired wait time
+
 (async () => {
   try {
     const allURLs = await scrapeURLs(URL, waitSec);
