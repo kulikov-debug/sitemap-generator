af2887490ab3a999aa905f2e09cc25af935cb4e4
kulikov-debug <63264840+kulikov-debug@users.noreply.github.com>
2023-08-11 14:53:14 +0200

    Update scrapingURLs.ts

--- src/script/scrapingURLs.ts
+++ src/script/scrapingURLs.ts
@@ -1,94 +1,77 @@
 import { launch, Page } from 'puppeteer';
 
-export const scrapeURLs = async (
-  initialURL: string,
-  waitSec?: number,
+export const scrapingURL = async (
+  URL: string,
+  waitSec: number = 3,
 ): Promise<string[]> => {
-  const browser = await launch({ headless: true }); // You can adjust the 'headless' option here
+  const browser = await launch({ headless: true });
   const page = await browser.newPage();
 
-  console.info('Start Scraping: ' + initialURL);
+  console.info('Start Scraping: ' + URL);
 
-  const allPageURLs = await getAllPageURLs(page, initialURL, waitSec ? waitSec : 3);
+  const allPageURLs = await getAllPageURLs(page, URL, waitSec);
   await browser.close();
   return allPageURLs;
 };
 
 const getAllPageURLs = async (
   page: Page,
-  initialURL: string,
+  URL: string,
   waitSec: number,
 ): Promise<string[]> => {
-  await page.goto(initialURL, { waitUntil: 'domcontentloaded' });
+  await page.goto(URL, { waitUntil: 'domcontentloaded' });
   await page.waitForTimeout(waitSec * 1000);
-  const currentPageURLs = await getCurrentPageURLs(page, initialURL);
-  const uniqueURLs = Array.from(new Set(currentPageURLs));
+  const currentPageURLs = await getCurrentPageURLs(page);
+  const currentURLs = currentPageURLs.map((path) => {
+    return /^(https|http):\/\//.test(path) ? path : (URL.endsWith('/') ? URL.slice(0, -1) : URL) + path;
+  });
+  const uniqueURLs = Array.from(new Set(currentURLs));
 
   console.info('Found URLs: ' + uniqueURLs.toString());
 
-  const allPageURLs = await getCurrentPageURLsRecursive(
+  return await getCurrentPageURLsRecursive(
     page,
-    initialURL,
+    URL,
     uniqueURLs,
     waitSec,
     [],
   );
-  return Array.from(new Set(allPageURLs));
 };
 
 const getCurrentPageURLsRecursive = async (
   page: Page,
-  initialURL: string,
+  URL: string,
   obtainedURLs: string[],
   waitSec: number,
   allPageURLs: string[],
-  count?: number,
+  count: number = 0,
 ): Promise<string[]> => {
-  const currentCount = count ? count : 0;
-  if (obtainedURLs.length === currentCount) {
-    return allPageURLs?.length ? allPageURLs : obtainedURLs;
+  if (obtainedURLs.length === count) {
+    return allPageURLs.length ? allPageURLs : obtainedURLs;
   }
 
-  console.info('Scraping: ' + obtainedURLs[currentCount]);
+  console.info('Scraping: ' + obtainedURLs[count]);
 
-  await page.goto(obtainedURLs[currentCount], {
-    waitUntil: 'domcontentloaded',
-  });
+  await page.goto(obtainedURLs[count], { waitUntil: 'domcontentloaded' });
   await page.waitForTimeout(waitSec * 1000);
-  const currentPageURLs = await getCurrentPageURLs(page, initialURL);
-  const uniqueURLs = Array.from(new Set(currentPageURLs));
-  allPageURLs.concat(uniqueURLs);
+  const currentPageURLs = await getCurrentPageURLs(page);
+  const currentURLs = currentPageURLs.map((path) => {
+    return /^(https|http):\/\//.test(path) ? path : (URL.endsWith('/') ? URL.slice(0, -1) : URL) + path;
+  });
+  const uniqueURLs = Array.from(new Set(currentURLs));
+  allPageURLs = [...allPageURLs, ...uniqueURLs];
   return await getCurrentPageURLsRecursive(
     page,
-    initialURL,
+    URL,
     obtainedURLs,
     waitSec,
     allPageURLs,
-    currentCount + 1,
+    count + 1,
   );
 };
 
-const getCurrentPageURLs = async (page: Page, baseURL: string): Promise<string[]> => {
-  const bodyHandle = await page.$('body');
-  return await page.evaluate((body, baseURL) => {
-    if (!body) {
-      return [];
-    }
-    const elements = Array.from(body.querySelectorAll('a'));
-    return elements
-      .map((element: Element) => {
-        let url = element?.getAttribute('href');
-        if (!url) {
-          return '';
-        }
-        if (/^(https|http):\/\//.test(url)) {
-          return url;
-        } else if (url.startsWith('/')) {
-          return baseURL + url;
-        } else {
-          return baseURL + '/' + url;
-        }
-      })
-      .filter((url) => url) as string[];
-  }, bodyHandle, baseURL);
+const getCurrentPageURLs = async (page: Page): Promise<string[]> => {
+  return await page.$$eval('a', anchors => {
+    return anchors.map(anchor => anchor.getAttribute('href')).filter(url => url);
+  });
 };
